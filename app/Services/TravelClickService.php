73db1dec1f8774a5adfb493ce53f18dca2b2892b
Kemel <studentnote@mail.ru>
2020-08-11 13:46:34 +0400

    hotel product

--- app/Services/TravelClickService.php
+++ app/Services/TravelClickService.php
@@ -4,18 +4,21 @@ namespace App\Services;
 
 class TravelClickService
 {
-    public function ping(array $data): array
+    private function success(array $data): array
     {
         $data['@attributes']['xmlns'] = 'http://www.opentravel.org/OTA/2003/05';
         $data['Success'] = null;
         return $data;
     }
 
+    public function ping(array $data): array
+    {
+        return $this->success($data);
+    }
+
     public function hotelProductRQ(array $data)
     {
-        $data['Success'] = null;
         unset($data['POS']);
-        $data['@attributes']['xmlns'] = 'http://www.opentravel.org/OTA/2003/05';
-        return $data;
+        return $this->success($data);
     }
 }
