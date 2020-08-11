466972328d85183b194be93dd51c4a0583ffe9dd
Kemel <studentnote@mail.ru>
2020-08-11 11:50:56 +0400

    post request in file

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/travelclick', function () {
-    $post = request()->post();
+    $post = request()->all();
     var_dump($post);
     $service= new \App\Services\XmlService();
     return $service->setContents('ping')->response();
